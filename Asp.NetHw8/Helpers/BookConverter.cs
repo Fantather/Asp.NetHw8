7b9aed10a8b67e0b17ca5219024198fe4e9c6ca9
Fantather <dirivolkova@gmail.com>
2026-02-21 21:59:20 +0200

    Добавил хелпер

---
+++ Asp.NetHw8/Helpers/BookConverter.cs
@@ -0,0 +1,17 @@
+﻿using Asp.NetHw8.InputModels;
+using Asp.NetHw8.Models;
+using Asp.NetHw8.Services;
+using Humanizer.Localisation;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Asp.NetHw8.Helpers
+{
+    public static class BookConverter
+    {
+        public async static Task<Book> ToBook(BookInputModel inputModel)
+        {
+            IEnumerable<string> imagePaths = inputModel.Images is null ? "" : await FileStorageService.SaveImagesAsync(inputModel.Images);
+            return new Book(inputModel.Title, inputModel.Author, inputModel.Genre, inputModel.Year, imagePaths);
+        }
+    }
+}
--- Asp.NetHw8/InputModels/BookInputModel.cs
+++ Asp.NetHw8/InputModels/BookInputModel.cs
@@ -3,12 +3,5 @@ using Asp.NetHw8.Services;
 
 namespace Asp.NetHw8.InputModels
 {
-    public record class BookInputModel(string Title, string Author, string Genre, int Year, List<IFormFile>? Images)
-    {
-        public async Task<Book> ToBook()
-        {
-            IEnumerable<string> imagePaths = Images is null ? "" : await FileStorageService.SaveImagesAsync(Images);
-            return new Book(Title, Author, Genre, Year, imagePaths);
-        }
-    }
+    public record class BookInputModel(string Title, string Author, string Genre, int Year, List<IFormFile>? Images);
 }
